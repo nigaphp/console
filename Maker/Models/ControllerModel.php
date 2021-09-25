cd12de091cf235cf65ba27f6004e52ead66f26ce
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-25 10:00:29 +0000

    refactor(controller) rename the class name - see [7f2e406](https://github.com/nigatedev/framework-bundle/commit/5988f2f2dc42247d9a1e90b0f08516e1c83fee3e)

--- Maker/Models/ControllerModel.php
+++ Maker/Models/ControllerModel.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Controller;
 
-use Nigatedev\FrameworkBundle\Controller\Controller;
+use Nigatedev\FrameworkBundle\Controller\AbstractController;
 use Nigatedev\FrameworkBundle\Http\Request;
 use Nigatedev\FrameworkBundle\Attributes\Route;
 
-class ControllerModel extends Controller
+class ControllerModel extends AbstractController
 {
     #[Route('/index')]
     public function index()
