564bc11aac37892af840625c2b65e6f4dbc8fa90
Abass Ben Cheik <cheikabassben@gmail.com>
2021-10-01 19:49:52 +0000

    update controller model

--- Maker/Models/ControllerModel.php
+++ Maker/Models/ControllerModel.php
@@ -2,13 +2,13 @@
 namespace App\Controller;
 
 use Nigatedev\FrameworkBundle\Controller\AbstractController;
-use Nigatedev\FrameworkBundle\Http\Request;
 use Nigatedev\FrameworkBundle\Attributes\Route;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ControllerModel extends AbstractController
 {
     #[Route('/index', name:'index')]
-    public function index()
+    public function index(Request $request)
     {
         return $this->render("index", [
         "cName" => "ControllerModel",
