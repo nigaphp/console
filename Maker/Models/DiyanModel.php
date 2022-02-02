7bda3e3a560185e866027cfc5b395aa47f8da323
Abass Ben Cheick <cheikabassben@gmail.com>
2022-02-02 19:03:48 +0000

    update: diyan template model

--- Maker/Models/DiyanModel.php
+++ Maker/Models/DiyanModel.php
@@ -1,2 +1,4 @@
-<h3>Hello <span class='be-color-py be-code'><?=$cName ?? "" ?></span></h3>
-<p>Your <span class='be-color-py be-code'><?=$cName ?? "" ?></span> class is located at <span class='be-color-py be-code'><?= $cPath ?? ""?></span> !</p>
+<?php if ($cName && $cPath) : ?>
+<h3>Hello <span class='be-color-py be-code'><?= $cName ?></span></h3>
+<p>Your <span class='be-color-py be-code'><?= $cName ?></span> class is located at <span class='be-color-py be-code'><?= $cPath ?></span> !</p>
+<?php endif ?>
