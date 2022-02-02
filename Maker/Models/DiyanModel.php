d87af0883d35e09345314f082149e90474a3387a
Abass Ben Cheick <cheikabassben@gmail.com>
2022-02-02 21:13:55 +0000

    fix: Undefined variable cName and

--- Maker/Models/DiyanModel.php
+++ Maker/Models/DiyanModel.php
@@ -1,4 +1,4 @@
-<?php if ($cName && $cPath) : ?>
+<?php if (isset($cName) && isset($cPath)) : ?>
 <h3>Hello <span class='be-color-py be-code'><?= $cName ?></span></h3>
 <p>Your <span class='be-color-py be-code'><?= $cName ?></span> class is located at <span class='be-color-py be-code'><?= $cPath ?></span> !</p>
 <?php endif ?>
